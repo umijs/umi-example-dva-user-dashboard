a4c148477d8f57b24e3794a68002c78d94707688
chencheng <sorrycc@gmail.com>
2018-02-22 15:45:05 +0800

    1. Initial commit

---
+++ src/models/example.js
@@ -0,0 +1,25 @@
+
+export default {
+
+  namespace: 'example',
+
+  state: {},
+
+  subscriptions: {
+    setup({ dispatch, history }) {
+    },
+  },
+
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      yield put({ type: 'save' });
+    },
+  },
+
+  reducers: {
+    save(state, action) {
+      return { ...state, ...action.payload };
+    },
+  },
+
+};

f12e32bbf75b52576a74f605adf5da7554ad40b4
chencheng <sorrycc@gmail.com>
2018-02-22 17:02:14 +0800

    9. Handle user delete

--- src/pages/users/services/users.js
+++ src/pages/users/services/users.js
@@ -4,3 +4,9 @@ import request from '../../../utils/request';
 export function fetch({ page = 1 }) {
   return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`);
 }
+
+export function remove(id) {
+  return request(`/api/users/${id}`, {
+    method: 'DELETE',
+  });
+}
